ce6f2b1581954fbfc17061893366b66af2aadef1
Tchipen <153777126+Tchipencode@users.noreply.github.com>
2024-05-03 12:43:46 +0100

    done

--- hashMap.mjs
+++ hashMap.mjs
@@ -1,101 +1,146 @@
-// class Node{
-//    constructor(key=null, value=null, next=null){
-//       this.key=key;
-//       this.value=value, 
-//       this.next=next;
-//    }
-// }
+class Node{
+   constructor(key=null, value=null, next=null){
+      this.key=key;
+      this.value=value, 
+      this.next=next;
+   }
+}
 class HashMap{
    constructor(){
       this.tables=new Array(16);
       this.size=0;
    }
+
    // Hash function to transform a key into hashcode
    hash(key) {
-      let hashCode = 0;
-         
+      let hashCode = 0;        
       const primeNumber = 31;
       for (let i = 0; i < key.length; i++) {
-        hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.tables.length;
-        
+        hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.tables.length;      
       }
       return hashCode; 
    } 
-   // Add a key-value pair to hash table
-   
+
+   // Add a key-value pair to hash table  
    set(key, value){
       const index= this.hash(key);
       if(!this.tables[index]){
-         this.tables[index]=[key, value];   
+         this.tables[index]=new Node(key, value);
+         this.size++;
+         return this.tables[index];    
       } else{
-         let table=this.tables[index];
-         table.push([key, value]);
-      }     
-      this.size++;   
-
+         let cur=this.tables[index];
+         while(cur.next!==null){
+            cur=cur.next;
+            
+         } 
+         cur.next=new Node(key, value); 
+         this.size++;        
+         return cur.next; 
+      }          
    }
+
    // Retrieve a value using key
    get(key){
       const index=this.hash(key);
-      for(let i=0; i<this.tables.length; i++){
-         if(this.tables[index][0]==key){
-            return this.tables[index][1];
-         } else{
-            return null;
+       
+      if(!this.tables[index]){
+         return null;
+      }
+      let cur=this.tables[index];
+      const result=[]; 
+      while(cur){
+         if(cur.key==key){
+            result.push(cur.value);
          }
+         cur=cur.next;                     
+      }
+      if(result.length!==0){
+         return result;
+      }
+      else{
+         return null;
       }
    }
+
    // Return true or false based on whether or not the key is in the hash map.
    has(key){
       const index=this.hash(key);
-      for(let i=0; i<this.tables.length; i++){
-         if(this.tables[index][0]==key){
+      let cur=this.tables[index];  
+      while(cur){
+         if(cur.key==key){
             return true;
-         } else{
-            return false;
          }
+         cur=cur.next;                     
       }
-
+      return false;
    }
+
    // Remove the entry with the given key and return true
    remove(key){
       const index=this.hash(key);
-      for(let i=0; i<this.tables.length; i++){
-         if(this.tables[index][0]==key){
-            this.tables.splice(index, 1);
-            this.size--;
-            return true;
-         } else{
-            return false;
-         }   
-      }
+      let cur=this.tables[index];
       
-   }
+      while(cur){
+         if(cur.key==key){
+            this.tables[index]=cur.next;
+               this.size--                 
+         }    
+         cur=cur.next;                          
+      }
+      if(!this.tables.includes(key)){
+         return true; 
+      }
+      else{
+         return false;
+      }
+   } 
+
    // returns the number of stored keys in the hash map.
    length(){
       return this.size;  
    }
+
    // removes all entries in the hash map.
    clear(){
       this.size=0;
       this.tables=new Array(16);
    }
+
    //  returns an array containing all the keys inside the hash map.
    keys(){
       let keyArr=[];
       for(let table of this.tables){
-         let cur=table.head;
-         for(let i=0; i<table.length; i++){
-            keyArr.push(cur.key);
-            cur=cur.next;
-         }
-      }
-      // for(let i=0; i<=this.tables.length; i++){
-      //    if(this.tables[i]){
-      //       keyArr.push(this.tables[i][0]);
-      //    } 
-      // }
+         while(table){
+            keyArr.push(table.key);
+            table=table.next;
+         }       
+      }   
       return keyArr;
+   }
+
+   //  returns an array containing all the values.
+   values(){
+      let valuesArr=[];
+      for(let table of this.tables){
+         while(table){
+            valuesArr.push(table.value);
+            table=table.next;
+         }       
+      }   
+      return valuesArr;
+   }
+
+   // returns an array that contains each key, value pair
+   entries(){
+      let entriesArr=[];
+      for(let table of this.tables){
+         while(table){
+            entriesArr.push([table.key, table.value]);
+            table=table.next;
+         }       
+      }   
+      return entriesArr;
    }
 }
 
@@ -103,13 +148,16 @@ const hashMap=new HashMap();
 console.log(hashMap.hash("john"));
 console.log(hashMap.set("john", "smith"));
 console.log(hashMap.set("john", "backer"));
+console.log(hashMap.set("hojn", "miller"));
 console.log(hashMap.set("lena", "manh"));
-console.log(hashMap.get("lena"));
-console.log(hashMap.has("lena"));
-console.log(hashMap.remove("lena"));
+console.log(hashMap.length());
+console.log(hashMap.get("maël"));
+console.log(hashMap.has("hojn"));
+// console.log(hashMap.remove("john"));
 console.log(hashMap.tables);
 console.log(hashMap.length());
 // console.log(hashMap.clear());
 console.log(hashMap.tables);
 console.log(hashMap.keys());
-// console.log(hashMap.tables);
+console.log(hashMap.values());
+console.log(hashMap.entries());
